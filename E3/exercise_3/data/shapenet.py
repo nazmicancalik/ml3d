f1dc52c9bfd37984babdaecbb51cc0618b32390f
Nazmican Çalık <nazmican@pm.me>
2021-06-01 00:02:26 +0200

    Threedepn is implemented

---
+++ E3/exercise_3/data/shapenet.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+import json
+
+import numpy as np
+import torch
+
+
+class ShapeNet(torch.utils.data.Dataset):
+    num_classes = 8
+    dataset_sdf_path = Path("exercise_3/data/shapenet_dim32_sdf")  # path to voxel data
+    dataset_df_path = Path("exercise_3/data/shapenet_dim32_df")  # path to voxel data
+    class_name_mapping = json.loads(Path("exercise_3/data/shape_info.json").read_text())  # mapping for ShapeNet ids -> names
+    classes = sorted(class_name_mapping.keys())
+
+    def __init__(self, split):
+        super().__init__()
+        assert split in ['train', 'val', 'overfit']
+        self.truncation_distance = 3
+
+        self.items = Path(f"exercise_3/data/splits/shapenet/{split}.txt").read_text().splitlines()  # keep track of shapes based on split
+
+    def __getitem__(self, index):
+        sdf_id, df_id = self.items[index].split(' ')
+
+        input_sdf = ShapeNet.get_shape_sdf(sdf_id)
+        target_df = ShapeNet.get_shape_df(df_id)
+
+        input_sdf = np.clip(input_sdf, a_min=-self.truncation_distance, a_max=self.truncation_distance)
+        input_sdf = np.stack([np.abs(input_sdf), np.sign(input_sdf)])  # (distances, sdf sign)
+        target_df = np.clip(target_df, a_min=0, a_max=self.truncation_distance)
+        target_df = np.log(np.add(target_df, 1.0))
+
+        return {
+            'name': f'{sdf_id}-{df_id}',
+            'input_sdf': input_sdf,
+            'target_df': target_df
+        }
+
+    def __len__(self):
+        return len(self.items)
+
+    @staticmethod
+    def move_batch_to_device(batch, device):
+        # TODO add code to move batch to device
+        pass
+
+    @staticmethod
+    def get_shape_sdf(shapenet_id):
+        sdf = None
+        # TODO implement sdf data loading
+        return sdf
+
+    @staticmethod
+    def get_shape_df(shapenet_id):
+        df = None
+        # TODO implement df data loading
+        return df
---
+++ E3/exercise_3/model/threedepn.py
@@ -0,0 +1,66 @@
+import torch
+import torch.nn as nn
+
+
+class ThreeDEPN(nn.Module):
+    def __init__(self):
+        super(ThreeDEPN, self).__init__()
+
+        self.num_features = 80
+
+        # TODO: 4 Encoder layers
+        self.conv1 = nn.Conv3d(32,16,4,stride=2,padding=1)
+        self.conv2 = nn.Conv3d(16,8,4,stride=2,padding=1)
+        self.conv3 = nn.Conv3d(8,4,4,stride=2,padding=1)
+        self.conv4 = nn.Conv3d(4,2,4,stride=1) # Last conv has no padding and stride=1
+
+        self.bn1 = nn.BatchNorm3d(8) # First layer doesn't get batch norm
+        self.bn2 = nn.BatchNorm3d(4)
+        self.bn3 = nn.BatchNorm3d(2)
+
+        self.leaky_relu = nn.LeakyReLU(0.2)
+
+        # TODO: 2 Bottleneck layers
+        self.bottleneck = nn.Sequential(
+            nn.Linear(self.num_features*8,self.num_features*8),
+            nn.ReLU(),
+            nn.Linear(self.num_features*8,self.num_features*8),
+            nn.ReLU()
+        )
+
+        # TODO: 4 Decoder layers
+        self.tconv1 = nn.ConvTranspose3d(self.num_features*8*2,self.num_features*8,4,stride=2,padding=1)
+        self.tconv2 = nn.ConvTranspose3d(self.num_features*8,self.num_features*4,4,stride=2,padding=1)
+        self.tconv3 = nn.ConvTranspose3d(self.num_features*4,self.num_features*2,4,stride=2,padding=1)
+        self.tconv4 = nn.ConvTranspose3d(self.num_features*2,self.num_features,4,stride=1)
+
+        self.bn4 = nn.BatchNorm3d(self.num_features*8)
+        self.bn5 = nn.BatchNorm3d(self.num_features*4)
+        self.bn6 = nn.BatchNorm3d(self.num_features*2) # No batch norm after last layer. Nor ReLU
+
+        self.relu = nn.ReLU()
+    def forward(self, x):
+        b = x.shape[0]
+        # Encode
+        x_e1 = self.leaky_relu(self.conv1(x))
+        x_e2 = self.leaky_relu(self.bn1(self.conv2(x_e1)))
+        x_e3 = self.leaky_relu(self.bn2(self.conv3(x_e2)))
+        x_e4 = self.leaky_relu(self.bn3(self.conv4(x_e3)))
+        
+        # TODO: Pass x though encoder while keeping the intermediate outputs for the skip connections
+        # Reshape and apply bottleneck layers
+        x = x_e4.view(b, -1)
+        x = self.bottleneck(x)
+        x = x.view(x.shape[0], x.shape[1], 1, 1, 1)
+        
+        # Decode
+        # TODO: Pass x through the decoder, applying the skip connections in the process
+        x = self.relu(self.bn4(self.tconv1(torch.cat((x,x_e4),dim=1)))) # Might need to change the dim later
+        x = self.relu(self.bn5(self.tconv2(torch.cat((x,x_e3),dim=1))))
+        x = self.relu(self.bn6(self.tconv3(torch.cat((x,x_e2),dim=1))))
+        x = self.tconv4(torch.cat((x,x_e1),dim=1))
+        
+        x = torch.squeeze(x, dim=1)
+        # TODO: Log scaling
+        x = torch.log(x+1)
+        return x
