a3b0e3202e3eaa9a0a7da6810231764b48455f0c
Nazmican Çalık <nazmican@pm.me>
2021-06-04 11:32:00 +0300

    deepsdf

--- E3/exercise_3/model/deepsdf.py
+++ E3/exercise_3/model/deepsdf.py
@@ -12,6 +12,21 @@ class DeepSDFDecoder(nn.Module):
         dropout_prob = 0.2
 
         # TODO: Define model
+        self.wn1 = nn.utils.weight_norm(nn.Linear(latent_size+3,512))
+        self.wn2 = nn.utils.weight_norm(nn.Linear(512,512))
+        self.wn3 = nn.utils.weight_norm(nn.Linear(512,512))
+        self.wn4 = nn.utils.weight_norm(nn.Linear(512,512-latent_size))
+
+        self.wn5 = nn.utils.weight_norm(nn.Linear(512,512))
+        self.wn6 = nn.utils.weight_norm(nn.Linear(512,512))
+        self.wn7 = nn.utils.weight_norm(nn.Linear(512,512))
+        self.wn8 = nn.utils.weight_norm(nn.Linear(512,512))
+
+        self.fc = nn.Linear(512,1)
+
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.2)
+        
 
     def forward(self, x_in):
         """
@@ -19,4 +34,16 @@ class DeepSDFDecoder(nn.Module):
         :return: B x 1 tensor
         """
         # TODO: implement forward pass
+        x = self.dropout(self.relu(self.wn1(x_in)))
+        x = self.dropout(self.relu(self.wn2(x)))
+        x = self.dropout(self.relu(self.wn3(x)))
+        x = self.dropout(self.relu(self.wn4(x)))
+        
+        x = torch.cat((x,x_in),dim=0)
+        x = self.dropout(self.relu(self.wn5(x)))
+        x = self.dropout(self.relu(self.wn6(x)))
+        x = self.dropout(self.relu(self.wn7(x)))
+        x = self.dropout(self.relu(self.wn8(x)))
+        
+        x = self.fc(x)
         return x
